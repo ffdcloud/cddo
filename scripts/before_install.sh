a8fd3763a661b82347d923409cedabc609f21284
Feb De La Cruz <fdelacruz@safehealth.me>
2024-12-11 18:26:47 -0700

    Update before_install script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,4 +7,14 @@ amazon-linux-extras enable php8.0
 yum install php php-cli php-mbstring php-xml php-soap php-zip php-mysqlnd -y
 yum install git -y
 yum install aws-cli unzip -y
+
+if ! command -v codedeploy-agent &>/dev/null; then
+  echo "Installing CodeDeploy agent..."
+  yum install -y codedeploy-agent
+else
+  echo "CodeDeploy agent is already installed."
+fi
+systemctl enable codedeploy-agent
+systemctl start codedeploy-agent
+
 rm -rf /var/www/html/*
